ca3c74443d526118caec12933eaf7dfcc42b2d31
TarifSadman <sadmantaha17@gmail.com>
2024-09-27 00:42:02 +0600

    AWS Amplify test

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,88 +1,28 @@
 import { useEffect, useRef, useState } from "react";
 import Isotope from "isotope-layout";
 
-const menuItems = [
-  {
-    id: 1,
-    imgSrc: "assets/img/menu/lobster-bisque.jpg",
-    title: "Lobster Bisque",
-    price: "$5.95",
-    description: "Lorem, deren, trataro, filede, nerada",
-    category: "filter-starters",
-  },
-  {
-    id: 2,
-    imgSrc: "assets/img/menu/bread-barrel.jpg",
-    title: "Bread Barrel",
-    price: "$6.95",
-    description: "Lorem, deren, trataro, filede, nerada",
-    category: "filter-specialty",
-  },
-  {
-    id: 3,
-    imgSrc: "assets/img/menu/cake.jpg",
-    title: "Crab Cake",
-    price: "$7.95",
-    description:
-      "A delicate crab cake served on a toasted roll with lettuce and tartar sauce",
-    category: "filter-starters",
-  },
-  {
-    id: 4,
-    imgSrc: "assets/img/menu/caesar.jpg",
-    title: "Caesar Selections",
-    price: "$8.95",
-    description: "Lorem, deren, trataro, filede, nerada",
-    category: "filter-salads",
-  },
-  {
-    id: 5,
-    imgSrc: "assets/img/menu/tuscan-grilled.jpg",
-    title: "Tuscan Grilled",
-    price: "$9.95",
-    description:
-      "Grilled chicken with provolone, artichoke hearts, and roasted red pesto",
-    category: "filter-specialty",
-  },
-  {
-    id: 6,
-    imgSrc: "assets/img/menu/mozzarella.jpg",
-    title: "Mozzarella Stick",
-    price: "$4.95",
-    description: "Lorem, deren, trataro, filede, nerada",
-    category: "filter-starters",
-  },
-  {
-    id: 7,
-    imgSrc: "assets/img/menu/greek-salad.jpg",
-    title: "Greek Salad",
-    price: "$9.95",
-    description: "Fresh spinach, crisp romaine, tomatoes, and Greek olives",
-    category: "filter-salads",
-  },
-  {
-    id: 8,
-    imgSrc: "assets/img/menu/spinach-salad.jpg",
-    title: "Spinach Salad",
-    price: "$9.95",
-    description:
-      "Fresh spinach with mushrooms, hard boiled egg, and warm bacon vinaigrette",
-    category: "filter-salads",
-  },
-  {
-    id: 9,
-    imgSrc: "assets/img/menu/lobster-roll.jpg",
-    title: "Lobster Roll",
-    price: "$12.95",
-    description:
-      "Plump lobster meat, mayo and crisp lettuce on a toasted bulky roll",
-    category: "filter-specialty",
-  },
-];
-
 export default function Menu() {
+  const [menuItems, setMenuItems] = useState([]);
   const [filter, setFilter] = useState("*");
   const isotopeContainerRef = useRef(null);
+
+  useEffect(() => {
+    const fetchMenuItems = async () => {
+      try {
+        const response = await fetch('http://localhost:3990/menu/menu-items');
+        const data = await response.json();
+        if (response.ok) {
+          setMenuItems(data.menuItems);
+        } else {
+          console.error('Failed to fetch menu items:', data.message);
+        }
+      } catch (error) {
+        console.error('Error fetching menu items:', error);
+      }
+    };
+
+    fetchMenuItems();
+  }, []);
 
   useEffect(() => {
     const isotope = new Isotope(isotopeContainerRef.current, {
@@ -93,11 +33,14 @@ export default function Menu() {
     isotope.arrange({ filter });
 
     return () => isotope.destroy();
-  }, [filter]);
+  }, [filter, menuItems]);
 
   const handleFilterChange = (newFilter) => {
     setFilter(newFilter);
   };
+
+  console.log("Amplify Test");
+  
 
   return (
     <section id="menu" className="menu section">
@@ -152,7 +95,7 @@ export default function Menu() {
               key={item.id}
               className={`col-lg-6 menu-item ${item.category}`}
             >
-              <img src={item.imgSrc} className="menu-img" alt={item.title} />
+              <img src={item.img_src} className="menu-img" alt={item.title} />
               <div className="menu-content">
                 <a href="#">{item.title}</a>
                 <span>{item.price}</span>
